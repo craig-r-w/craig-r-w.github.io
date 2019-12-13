bf9cf31d66ec831f4f58c8f0f946bbff13fa9653
craig-r-w <56520435+craig-r-w@users.noreply.github.com>
2019-12-13 17:21:32 +0000

    change

--- service_worker.js
+++ service_worker.js
@@ -1,5 +1,9 @@
 
 //Only runs when the service worker has been physically changed or when a brand new one is created.
 self.addEventListener( "install", (e) => {
-	console.log("installing");
+	console.log("Service Worker: Installing.");
+});
+
+self.addEventListener( "activate", (e) => {
+	console.log("Service Worker: Activated.");
 });

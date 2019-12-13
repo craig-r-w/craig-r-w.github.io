0c70d496f5f355ba10a0fac9b91164328d3abdc2
craig-r-w <56520435+craig-r-w@users.noreply.github.com>
2019-12-13 12:23:59 +0000

    Notfication changes

--- service_worker.js
+++ service_worker.js
@@ -1,2 +1,5 @@
 
-console.log("WORK");
+//Only runs when the service worker has been physically changed or when a brand new one is created.
+self.addEventListener( "install", (e) => {
+	console.log("installing");
+});

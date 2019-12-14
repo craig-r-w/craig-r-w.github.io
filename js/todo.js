8dfd9ea92a8f1a672e19de9efb2584e3f2efc678
craig-r-w <56520435+craig-r-w@users.noreply.github.com>
2019-12-14 14:35:35 +0000

    Added css grid

--- js/todo.js
+++ js/todo.js
@@ -1,5 +1,33 @@
 console.log("TODO");
 
+/*
 window.addEventListener("load", (e) => {
-	console.log("Page Loaded.");
+	var container = document.createElement("div");
+	container.setAttribute("id", "container");
+	document.body.appendChild(container);
+	
+	var list = document.createElement("ul");
+	list.setAttribute("id", "display");
+	document.getElementById("container").appendChild(list);
+	
+	var editor = document.createElement("div");
+	editor.setAttribute("id", "editor");
+	document.getElementById("container").appendChild(editor);
+	
+	var title = document.createElement("input");
+	title.setAttribute("id", "title");
+	title.setAttribute("type", "text");
+	title.setAttribute("autocomplete", "off");
+	document.getElementById("editor").appendChild(title);
+	
+	var description = document.createElement("textarea");
+	description.setAttribute("id", "description");
+	document.getElementById("editor").appendChild(description);
+	
+	var add = document.createElement("input");
+	add.setAttribute("id", "add");
+	add.setAttribute("type", "submit");
+	add.setAttribute("value", "+");
+	document.getElementById("editor").appendChild(add);
 });
+*/

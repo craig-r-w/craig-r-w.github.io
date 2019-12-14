4cba60077ca8e5c1aa47b8d2870724a0ad11995c
craig-r-w <56520435+craig-r-w@users.noreply.github.com>
2019-12-14 11:48:11 +0000

    Update service_worker.js

--- service_worker.js
+++ service_worker.js
@@ -1,9 +1,15 @@
 
 //Only runs when the service worker has been physically changed or when a brand new one is created.
 self.addEventListener( "install", (e) => {
+	//Add all the rerlevant files to the cache.
 	console.log("Service Worker: Installing.");
 });
 
 self.addEventListener( "activate", (e) => {
 	console.log("Service Worker: Activated.");
+});
+
+self.addEventListener( "fetch", (e) => {
+	//TODO add Cache.match
+	console.log("Service Worker: Fetching resource.");
 });
